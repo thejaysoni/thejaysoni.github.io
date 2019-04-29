37104794c429b79621e49bf21e3f7d164ef59bea
Jay soni <jaysoni100@gmail.com>
2019-04-29 14:18:27 +0530

    changes

--- htm_boilerplate/scripts/myjs.js
+++ htm_boilerplate/scripts/myjs.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    strings: [
+     
+      "hello",
+      "this is amazing" ,"नमस्ते"   ],
+      
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true,
+  });
   console.log("hello")
 });
